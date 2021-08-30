e1817d71d38786d1dc291392bfa1c2773dc54709
larissa <larissa-akemi10@hotmail.com>
2021-08-30 12:56:48 -0400

    finalização da header

--- App.tsx
+++ App.tsx
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { BackHandler, ImageBackgroundBase, StyleSheet, Text, View, Image, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+    <View style={styles.header}>
+        <Image source={
+            require("./src/assets/seta.png")
+            }>
+        </Image>
+        <View style={styles.inputTextView}>
+            <Image source={
+                require("./src/assets/ei_search.png")
+                }>
+            </Image>
+            <TextInput
+                placeholder="Search"
+                placeholderTextColor="#F5FFFF"
+                style={styles.textInput}
+                >
+
+            </TextInput>
+        </View>
+        <Image source={
+            require("./src/assets/share.png")
+            }>
+        </Image>
+        
+    </View>
       <Text>Taylor Swift IS the music industry</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,9 +37,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex:1,
+  },
+  header:{
+      minHeight:93,
+      backgroundColor: "#4369B0",
+      paddingTop:40,
+      flexDirection:"row",
+      justifyContent:"space-between",
+      paddingHorizontal:30,
+  },
+  inputTextView:{
+      flex:1,
+      backgroundColor:"#2C4877",
+      height:40,
+      marginHorizontal:10,
+      alignItems: "center",
+      borderRadius:28,
+      flexDirection:"row"
+  },
+  textInput:{
+      color:"#F5FFFF",
+      // maxWidth:200
+      flex:1,
+      paddingRight:10,
   },
 });
